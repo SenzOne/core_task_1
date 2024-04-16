b09469f70dbe924254248430da9aeec77dbbfe88
Simeon Slashchinin <sem63sam@yandex.ru>
2024-04-16 21:48:43 +0400

    Initial commit

---
+++ src/main/java/com/example/core_task_1/CoreTask1Application.java
@@ -0,0 +1,13 @@
+package com.example.core_task_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreTask1Application.class, args);
+    }
+
+}
